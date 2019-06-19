1632c15e0ebbd2bef2ef667db8d2b87732dc3942
Oleh <olkozhushko@gmail.com>
2019-06-19 15:54:39 +0300

    Updates

---
+++ precache-manifest.dd0b61f3baeb14b0e0d2c7ee3ee03102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d0d377afcce747ecf52ddb0d70c467",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "b7ecdca9d8b05b5cd80c",
+    "url": "/react-pagination/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6aee650de6a8bfb8b162",
+    "url": "/react-pagination/static/css/main.6ebdf05c.chunk.css"
+  },
+  {
+    "revision": "b7ecdca9d8b05b5cd80c",
+    "url": "/react-pagination/static/js/2.96458c22.chunk.js"
+  },
+  {
+    "revision": "6aee650de6a8bfb8b162",
+    "url": "/react-pagination/static/js/main.9cc94228.chunk.js"
+  },
+  {
+    "revision": "6bb6875cc2e032a389e0",
+    "url": "/react-pagination/static/js/runtime~main.f8e684e0.js"
+  }
+]);
